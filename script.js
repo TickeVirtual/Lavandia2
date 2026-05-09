2629db216b2f4af6175e6be9c20f1b766259e855
SistemApp <147223054+TickeVirtual@users.noreply.github.com>
2026-05-09 18:57:03 -0500

    Restore URL parameter retrieval and processing code

--- script.js
+++ script.js
@@ -233,7 +233,7 @@ statusMessage.className = 'loading';
 
 sendMessageButton.disabled = true; // Deshabilitar botón mientras se envía
         const url = "https://mensajero-evolution-api.ykf6ye.easypanel.host/message/sendMedia/lavandiasanborjainstancia"; // Cambia NOMBRE_INSTANCIA
-        const apikey = "C343AC87AA49-4A0C-AF93-F2623CBBFCB8"; // Coloca aquí tu API key
+        const apikey = "0hvb8lbwm92"; // Coloca aquí tu API key
         const numeroTelefono = `+51${telefono}`; // Coloca el número de teléfono del destinatario
         const longURL = window.location.href; // Obtiene la URL actual
 
